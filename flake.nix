143c2a67b6cf3e91887d824399aba21086a87096
Mateusz Jakobsche <mateusz.jakobsche@buddy.works>
2024-05-21 14:11:21 +0200

    project

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Nix-flake for ionic/android app";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      overlays = [
+        (final: prev: rec {
+          nodejs = prev.nodejs-18_x;
+        })
+      ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit overlays system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ nodejs typescript rbenv bundler fastlane bundletool ];
+        };
+      });
+    };
+}
