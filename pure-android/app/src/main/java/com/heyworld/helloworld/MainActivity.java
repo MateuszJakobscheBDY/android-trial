397f99a5d9ab8f2a1ec1348e6d5e355b3456d41d
Mateusz Jakobsche <mateusz.jakobsche@buddy.works>
2024-05-23 16:50:35 +0200

    change app

--- pure-android/app/src/main/java/com/heyworld/helloworld/MainActivity.java
+++ pure-android/app/src/main/java/com/heyworld/helloworld/MainActivity.java
@@ -3,6 +3,8 @@ package com.heyworld.helloworld;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import com.example.helloworld.R;
+import com.example.helloworld.databinding.ActivityMainBinding;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 import androidx.navigation.NavController;
@@ -11,7 +13,6 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
-import com.heyworld.helloworld.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
